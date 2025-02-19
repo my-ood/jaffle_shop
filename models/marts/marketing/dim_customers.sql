9adf735308413e968922929c4112d6bdeb0bf2b7
Alex Varbanov <alexvurbanov@gmail.com>
2025-02-19 22:54:46 +0000

    refactor sources

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -6,6 +6,10 @@ orders as (
     select * from {{ ref('stg_jaffle_shop__orders')}}    
 ),
 
+fct_orders as (
+    select * from {{ ref("fct_orders")}}
+),
+
 customer_orders as (
     select
         customer_id,
@@ -19,6 +23,15 @@ customer_orders as (
 
 ),
 
+customer_payments as (
+    select 
+        customer_id,
+        coalesce(sum(amount), 0) AS lifetime_value  
+    from fct_orders
+    group by customer_id
+)
+,
+
 final as (
     select  
         customers.customer_id,
@@ -26,10 +39,12 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_payments.lifetime_value, 0) as lifetime_value
     
     from customers
     left join customer_orders using (customer_id)
+    left join customer_payments using (customer_id)
 )
 
 select * from final
